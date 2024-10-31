73e3e49b6463527d5b42ea237e5a4b6cb3ca9583
Mikołaj Zakrzewski <mikizakr@gmail.com>
2024-10-31 23:42:15 +0100

    Implemented AbstractMongoRepository

--- src/main/java/edu/nbd/repositories/AbstractMongoRepository.java
+++ src/main/java/edu/nbd/repositories/AbstractMongoRepository.java
@@ -6,24 +6,27 @@ import com.mongodb.MongoCredential;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
+import edu.nbd.model.ClientTypeCodec;
 import org.bson.UuidRepresentation;
 import org.bson.codecs.configuration.CodecRegistries;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.Conventions;
 import org.bson.codecs.pojo.PojoCodecProvider;
 
-import java.util.List;
-
 public abstract class AbstractMongoRepository implements AutoCloseable {
 
     private ConnectionString connectionString = new ConnectionString("mongodb://mongodb1:27017,mongodb2:27017,mongodb3:27017/?replicaSet=replica_set_single");
     private MongoCredential credential = MongoCredential.createCredential("nbd", "admin", "nbdpassword".toCharArray());
 
-    private CodecRegistry pojoCodecRegistry = CodecRegistries.fromProviders(
-            PojoCodecProvider.builder()
-                    .automatic(true)
-                    .conventions(List.of(Conventions.ANNOTATION_CONVENTION))
-                    .build()
+    private CodecRegistry pojoCodecRegistry = CodecRegistries.fromRegistries(
+            MongoClientSettings.getDefaultCodecRegistry(),
+            CodecRegistries.fromCodecs(new ClientTypeCodec()),
+            CodecRegistries.fromProviders(
+                PojoCodecProvider.builder()
+                        .automatic(true)
+                        .conventions(Conventions.DEFAULT_CONVENTIONS)
+                        .build()
+            )
     );
 
     private MongoClient mongoClient;
@@ -39,5 +42,19 @@ public abstract class AbstractMongoRepository implements AutoCloseable {
 
         mongoClient = MongoClients.create(settings);
         database = mongoClient.getDatabase("nbddb");
+    }
+
+    public MongoDatabase getDatabase() {
+        if (database == null) {
+            initDbConnection();
+        }
+        return database;
+    }
+
+    @Override
+    public void close() {
+        if (mongoClient != null) {
+            mongoClient.close();
+        }
     }
 }
