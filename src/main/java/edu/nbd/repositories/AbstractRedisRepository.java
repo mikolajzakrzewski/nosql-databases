66a6e003bdeddcdeaab632f1c64465a2a8af6277
Mikołaj Zakrzewski <mikizakr@gmail.com>
2024-11-25 20:07:39 +0100

    AbstractRedisRepository (not tested)

---
+++ src/main/java/edu/nbd/repositories/AbstractRedisRepository.java
@@ -0,0 +1,36 @@
+package edu.nbd.repositories;
+
+import redis.clients.jedis.DefaultJedisClientConfig;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisClientConfig;
+import redis.clients.jedis.JedisPooled;
+
+import java.util.ResourceBundle;
+
+public abstract class AbstractRedisRepository implements AutoCloseable {
+
+    private static JedisPooled pool;
+
+    public void initDbConnection() {
+        ResourceBundle rb = ResourceBundle.getBundle("redis");
+        String host = rb.getString("redis.host");
+        int port = Integer.parseInt(rb.getString("redis.port"));
+
+        JedisClientConfig clientConfig = DefaultJedisClientConfig.builder().build();
+        pool = new JedisPooled(new HostAndPort(host, port), clientConfig);
+    }
+
+    public JedisPooled getPool() {
+        if (pool == null) {
+            initDbConnection();
+        }
+        return pool;
+    }
+
+    @Override
+    public void close() {
+        if (pool != null) {
+            pool.close();
+        }
+    }
+}
